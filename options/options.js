5b230f50b7fabe981decfb4441f497e64173ca37
Crowdbotics <engineering@crowdbotics.com>
2021-08-19 17:10:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "test_2_2Identifier",
+  url: "https://test_2_2Identifier.botics.co",
+  api: "https://test_2_2Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
